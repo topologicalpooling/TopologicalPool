fa19ff503b9c46a808f5171e95cf2aa7efcebec7
Yuzhou Chen <yuzhouchen888@gmail.com>
2023-03-25 16:04:18 -0400

    Wit-TopoPool

---
+++ Molecular Dataset/models/witness_graphcnn.py
@@ -0,0 +1,342 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn.pool.topk_pool import topk,filter_adj
+from torch_geometric.nn import GCNConv, SAGEConv, GATConv, GINConv
+from torch_geometric.utils.num_nodes import maybe_num_nodes
+from torch_geometric.utils import to_dense_adj
+
+import sys
+sys.path.append("models/")
+from mlp import MLP
+
+from torch.autograd import Variable
+import pdb
+import gudhi as gd
+import numpy as np
+
+def persistence_images(dgm, resolution = [5,5], return_raw = False, normalization = True, bandwidth = 1., power = 1.):
+    PXs, PYs = dgm[:, 0], dgm[:, 1]
+    xm, xM, ym, yM = PXs.min(), PXs.max(), PYs.min(), PYs.max()
+    x = np.linspace(xm, xM, resolution[0])
+    y = np.linspace(ym, yM, resolution[1])
+    X, Y = np.meshgrid(x, y)
+    Zfinal = np.zeros(X.shape)
+    X, Y = X[:, :, np.newaxis], Y[:, :, np.newaxis]
+
+    # Compute persistence image
+    P0, P1 = np.reshape(dgm[:, 0], [1, 1, -1]), np.reshape(dgm[:, 1], [1, 1, -1])
+    weight = np.abs(P1 - P0)
+    distpts = np.sqrt((X - P0) ** 2 + (Y - P1) ** 2)
+
+    if return_raw:
+        lw = [weight[0, 0, pt] for pt in range(weight.shape[2])]
+        lsum = [distpts[:, :, pt] for pt in range(distpts.shape[2])]
+    else:
+        weight = weight ** power
+        Zfinal = (np.multiply(weight, np.exp(-distpts ** 2 / bandwidth))).sum(axis=2)
+
+    output = [lw, lsum] if return_raw else Zfinal
+
+    if normalization:
+        norm_output = (output - np.min(output))/(np.max(output) - np.min(output))
+    else:
+        norm_output = output
+
+    return norm_output
+
+def diagram_from_simplex_tree(st, mode, dim=0):
+    st.compute_persistence(min_persistence=-1.)
+    dgm0 = st.persistence_intervals_in_dimension(0)[:, 1]
+
+    if mode == "superlevel":
+        dgm0 = - dgm0[np.where(np.isfinite(dgm0))]
+    elif mode == "sublevel":
+        dgm0 = dgm0[np.where(np.isfinite(dgm0))]
+    if dim==0:
+        return dgm0
+    elif dim==1:
+        dgm1 = st.persistence_intervals_in_dimension(1)[:,0]
+        return dgm0, dgm1
+
+def sum_diag_from_point_cloud(X, mode="superlevel"):
+    rc = gd.RipsComplex(points=X)
+    st = rc.create_simplex_tree(max_dimension=1)
+    dgm = diagram_from_simplex_tree(st, mode=mode)
+    sum_dgm = np.sum(dgm)
+    return sum_dgm
+
+
+class GraphCNN(nn.Module):
+    def __init__(self, num_layers, num_mlp_layers, input_dim, hidden_dim, output_dim, final_dropout, learn_eps, graph_pooling_type, neighbor_pooling_type, device):
+        '''
+            num_layers: number of layers in the neural networks (INCLUDING the input layer)
+            num_mlp_layers: number of layers in mlps (EXCLUDING the input layer)
+            input_dim: dimensionality of input features
+            hidden_dim: dimensionality of hidden units at ALL layers
+            output_dim: number of classes for prediction
+            final_dropout: dropout ratio on the final linear layer
+            learn_eps: If True, learn epsilon to distinguish center nodes from neighboring nodes. If False, aggregate neighbors and center nodes altogether. 
+            neighbor_pooling_type: how to aggregate neighbors (mean, average, or max)
+            graph_pooling_type: how to aggregate entire nodes in a graph (mean, average)
+            device: which device to use
+        '''
+
+        super(GraphCNN, self).__init__()
+
+        self.final_dropout = final_dropout
+        self.device = device
+        self.num_layers = num_layers
+        self.graph_pooling_type = graph_pooling_type
+        self.neighbor_pooling_type = neighbor_pooling_type
+        self.learn_eps = learn_eps
+        self.eps = nn.Parameter(torch.zeros(self.num_layers-1))
+        self.num_neighbors = 5
+
+        ###List of MLPs
+        self.mlps = torch.nn.ModuleList()
+
+        ###List of batchnorms applied to the output of MLP (input of the final prediction linear layer)
+        self.batch_norms = torch.nn.ModuleList()
+
+        for layer in range(self.num_layers-1):
+            if layer == 0:
+                self.mlps.append(MLP(num_mlp_layers, input_dim, hidden_dim, hidden_dim))
+            else:
+                self.mlps.append(MLP(num_mlp_layers, hidden_dim, hidden_dim, hidden_dim))
+
+            self.batch_norms.append(nn.BatchNorm1d(hidden_dim))
+
+        # for attentional second-order pooling
+        self.PI_hidden_dim = 16
+        self.total_latent_dim = input_dim + hidden_dim * (num_layers - 1) + self.PI_hidden_dim
+        self.dense_dim = self.total_latent_dim
+        self.attend = nn.Linear(self.total_latent_dim - self.PI_hidden_dim, 1)
+        self.linear1 = nn.Linear(self.dense_dim, output_dim)
+        self.mlp_PI_witnesses = nn.Linear(25, self.PI_hidden_dim)
+
+        # point clouds pooling for nodes
+        self.score_node_layer = GCNConv(input_dim, self.num_neighbors * 2)
+        # point clouds pooling for graphs
+        self.score_graph_layer = GCNConv(input_dim, 2)
+
+
+    def __preprocess_neighbors_maxpool(self, batch_graph):
+        ###create padded_neighbor_list in concatenated graph
+
+        #compute the maximum number of neighbors within the graphs in the current minibatch
+        max_deg = max([graph.max_neighbor for graph in batch_graph])
+
+        padded_neighbor_list = []
+        start_idx = [0]
+
+
+        for i, graph in enumerate(batch_graph):
+            start_idx.append(start_idx[i] + len(graph.g))
+            padded_neighbors = []
+            for j in range(len(graph.neighbors)):
+                #add off-set values to the neighbor indices
+                pad = [n + start_idx[i] for n in graph.neighbors[j]]
+                #padding, dummy data is assumed to be stored in -1
+                pad.extend([-1]*(max_deg - len(pad)))
+
+                #Add center nodes in the maxpooling if learn_eps is False, i.e., aggregate center nodes and neighbor nodes altogether.
+                if not self.learn_eps:
+                    pad.append(j + start_idx[i])
+
+                padded_neighbors.append(pad)
+            padded_neighbor_list.extend(padded_neighbors)
+
+        return torch.LongTensor(padded_neighbor_list)
+
+
+    def __preprocess_neighbors_sumavepool_witnesses(self, batch_graph):
+        ###create block diagonal sparse matrix
+        edge_attr = None
+
+        edge_mat_list = []
+        start_idx = [0]
+        pooled_x = []
+        pooled_graph_sizes = []
+        PI_witnesses_dgms = []
+        for i, graph in enumerate(batch_graph):
+            x = graph.node_features.to(self.device)
+            edge_index = graph.edge_mat.to(self.device)
+
+            witnesses = self.score_graph_layer(x, edge_index).to(self.device)
+            node_embeddings = self.score_node_layer(x, edge_index).to(self.device)
+            node_point_clouds = node_embeddings.view(-1, self.num_neighbors, 2).to(self.device)
+            score_lifespan = torch.FloatTensor([sum_diag_from_point_cloud(node_point_clouds[i,...]) for i in range(node_point_clouds.size(0))]).to(self.device)
+
+            batch = torch.LongTensor([0] * x.size(0)).to(self.device)
+            perm = topk(score_lifespan, 0.5, batch)
+            x = x[perm]
+            edge_index, _ = filter_adj(edge_index, edge_attr, perm, num_nodes=graph.node_features.size(0))
+
+            # witnesses complex
+            network_statistics = torch.sum(to_dense_adj(graph.edge_mat)[0,:,:], dim = 1).to(self.device)
+            witnesses_perm = topk(network_statistics, 10, batch)
+            landmarks = witnesses[witnesses_perm]
+            witness_complex = gd.EuclideanStrongWitnessComplex(witnesses=witnesses, landmarks=landmarks)
+            simplex_tree = witness_complex.create_simplex_tree(max_alpha_square = 1, limit_dimension = 1)
+            simplex_tree.compute_persistence(min_persistence=-1.)
+            witnesses_dgm = simplex_tree.persistence_intervals_in_dimension(0)[:-1,:]
+            # persistence image based on witnesses diagram
+            PI_witnesses_dgm = torch.FloatTensor(persistence_images(witnesses_dgm, normalization = False).reshape(1,-1)).to(self.device) # vectorization
+            PI_witnesses_dgms.append(PI_witnesses_dgm)
+
+            start_idx.append(start_idx[i] + x.size(0))
+            edge_mat_list.append(edge_index + start_idx[i])
+            pooled_x.append(x)
+            pooled_graph_sizes.append(x.size(0))
+
+        pooled_X_concat = torch.cat(pooled_x, 0).to(self.device)
+        Adj_block_idx = torch.cat(edge_mat_list, 1).to(self.device)
+        Adj_block_elem = torch.ones(Adj_block_idx.shape[1]).to(self.device)
+
+        if not self.learn_eps:
+            num_node = start_idx[-1]
+            self_loop_edge = torch.LongTensor([range(num_node), range(num_node)]).to(self.device)
+            elem = torch.ones(num_node).to(self.device)
+            Adj_block_idx = torch.cat([Adj_block_idx, self_loop_edge], 1).to(self.device)
+            Adj_block_elem = torch.cat([Adj_block_elem, elem], 0).to(self.device)
+
+        Adj_block = torch.sparse.FloatTensor(Adj_block_idx, Adj_block_elem, torch.Size([start_idx[-1],start_idx[-1]]))
+
+
+        return Adj_block.to(self.device), pooled_X_concat, PI_witnesses_dgms, pooled_graph_sizes
+
+
+    def __preprocess_graphpool(self, batch_graph):
+        ###create sum or average pooling sparse matrix over entire nodes in each graph (num graphs x num nodes)
+        
+        start_idx = [0]
+
+        #compute the padded neighbor list
+        for i, graph in enumerate(batch_graph):
+            start_idx.append(start_idx[i] + len(graph.g))
+
+        idx = []
+        elem = []
+        for i, graph in enumerate(batch_graph):
+            ###average pooling
+            if self.graph_pooling_type == "average":
+                elem.extend([1./len(graph.g)]*len(graph.g))
+            
+            else:
+            ###sum pooling
+                elem.extend([1]*len(graph.g))
+
+            idx.extend([[i, j] for j in range(start_idx[i], start_idx[i+1], 1)])
+        elem = torch.FloatTensor(elem)
+        idx = torch.LongTensor(idx).transpose(0,1)
+        graph_pool = torch.sparse.FloatTensor(idx, elem, torch.Size([len(batch_graph), start_idx[-1]]))
+        
+        return graph_pool.to(self.device)
+
+    def maxpool(self, h, padded_neighbor_list):
+        ###Element-wise minimum will never affect max-pooling
+
+        dummy = torch.min(h, dim = 0)[0]
+        h_with_dummy = torch.cat([h, dummy.reshape((1, -1)).to(self.device)])
+        pooled_rep = torch.max(h_with_dummy[padded_neighbor_list], dim = 1)[0]
+        return pooled_rep
+
+
+    def next_layer_eps(self, h, layer, padded_neighbor_list = None, Adj_block = None):
+        ###pooling neighboring nodes and center nodes separately by epsilon reweighting. 
+
+        if self.neighbor_pooling_type == "max":
+            ##If max pooling
+            pooled = self.maxpool(h, padded_neighbor_list)
+        else:
+            #If sum or average pooling
+            pooled = torch.spmm(Adj_block, h)
+            if self.neighbor_pooling_type == "average":
+                #If average pooling
+                degree = torch.spmm(Adj_block, torch.ones((Adj_block.shape[0], 1)).to(self.device))
+                pooled = pooled/degree
+
+        #Reweights the center node representation when aggregating it with its neighbors
+        pooled = pooled + (1 + self.eps[layer])*h
+        pooled_rep = self.mlps[layer](pooled)
+        h = self.batch_norms[layer](pooled_rep)
+
+        #non-linearity
+        h = F.relu(h)
+        return h
+
+
+    def next_layer(self, h, layer, padded_neighbor_list = None, Adj_block = None):
+        ###pooling neighboring nodes and center nodes altogether  
+            
+        if self.neighbor_pooling_type == "max":
+            ##If max pooling
+            pooled = self.maxpool(h, padded_neighbor_list)
+        else:
+            #If sum or average pooling
+            pooled = torch.spmm(Adj_block, h) # ******
+            if self.neighbor_pooling_type == "average":
+                #If average pooling
+                degree = torch.spmm(Adj_block, torch.ones((Adj_block.shape[0], 1)).to(self.device))
+                pooled = pooled/degree
+
+        #representation of neighboring and center nodes 
+        pooled_rep = self.mlps[layer](pooled)
+
+        h = self.batch_norms[layer](pooled_rep)
+
+        #non-linearity
+        h = F.relu(h)
+        return h
+
+
+    def forward(self, batch_graph):
+        if self.neighbor_pooling_type == "max":
+            padded_neighbor_list = self.__preprocess_neighbors_maxpool(batch_graph)
+        else:
+            Adj_block, pooled_X_concat, PI_witnesses_dgms, pooled_graph_sizes = self.__preprocess_neighbors_sumavepool_witnesses(batch_graph)
+
+        #list of hidden representation at each layer (including input)
+        hidden_rep = [pooled_X_concat]
+        h = pooled_X_concat
+
+        for layer in range(self.num_layers-1):
+            if self.neighbor_pooling_type == "max" and self.learn_eps:
+                h = self.next_layer_eps(h, layer, padded_neighbor_list = padded_neighbor_list)
+            elif not self.neighbor_pooling_type == "max" and self.learn_eps:
+                h = self.next_layer_eps(h, layer, Adj_block = Adj_block)
+            elif self.neighbor_pooling_type == "max" and not self.learn_eps:
+                h = self.next_layer(h, layer, padded_neighbor_list = padded_neighbor_list)
+            elif not self.neighbor_pooling_type == "max" and not self.learn_eps:
+                # operation
+                h = self.next_layer(h, layer, Adj_block = Adj_block)
+
+            hidden_rep.append(h)
+
+        hidden_rep = torch.cat(hidden_rep, 1)
+
+        # after graph pooling
+        graph_sizes = pooled_graph_sizes
+
+        batch_graphs = torch.zeros(len(graph_sizes), self.dense_dim - self.PI_hidden_dim).to(self.device)
+        batch_graphs = Variable(batch_graphs)
+
+        batch_graphs_out = torch.zeros(len(graph_sizes), self.dense_dim).to(self.device)
+        batch_graphs_out = Variable(batch_graphs_out)
+
+
+        node_embeddings = torch.split(hidden_rep, graph_sizes, dim=0)
+
+        for g_i in range(len(graph_sizes)):
+            cur_node_embeddings = node_embeddings[g_i]
+            attn_coef = self.attend(cur_node_embeddings)
+            attn_weights = torch.transpose(attn_coef, 0, 1)
+            cur_graph_embeddings = torch.matmul(attn_weights, cur_node_embeddings)
+            batch_graphs[g_i] = cur_graph_embeddings.view(self.dense_dim - self.PI_hidden_dim)
+            witnesses_PI_out = (self.mlp_PI_witnesses(PI_witnesses_dgms[g_i])).view(-1)
+            batch_graphs_out[g_i] = torch.cat([batch_graphs[g_i], witnesses_PI_out], dim=0)
+
+        score = F.dropout(self.linear1(batch_graphs_out), self.final_dropout, training=self.training)
+
+        return score
---
+++ Molecular Dataset/util.py
@@ -0,0 +1,209 @@
+import networkx as nx
+import numpy as np
+import random
+import torch
+from sklearn.model_selection import StratifiedKFold
+from torch_geometric.datasets import TUDataset
+from torch_geometric.utils import degree
+import torch_geometric.transforms as T
+from torch_geometric.utils.convert import to_networkx
+import pickle
+import time
+
+class S2VGraph(object):
+    def __init__(self, g, label, node_tags=None, node_features=None):
+        '''
+            g: a networkx graph
+            label: an integer graph label
+            node_tags: a list of integer node tags
+            node_features: a torch float tensor, one-hot representation of the tag that is used as input to neural nets
+            edge_mat: a torch long tensor, contain edge list, will be used to create torch sparse tensor
+            neighbors: list of neighbors (without self-loop)
+        '''
+        self.label = label
+        self.g = g
+        self.node_tags = node_tags
+        self.neighbors = []
+        self.node_features = 0
+        self.edge_mat = 0
+
+        self.max_neighbor = 0
+
+
+def load_data(dataset, degree_as_tag):
+    '''
+        dataset: name of dataset
+        test_proportion: ratio of test train split
+        seed: random seed for random splitting of dataset
+    '''
+
+    print('loading data')
+    g_list = []
+    label_dict = {}
+    feat_dict = {}
+
+    with open('dataset/%s/%s.txt' % (dataset, dataset), 'r') as f:
+        n_g = int(f.readline().strip())
+        for i in range(n_g):
+            row = f.readline().strip().split()
+            n, l = [int(w) for w in row]
+            if not l in label_dict:
+                mapped = len(label_dict)
+                label_dict[l] = mapped
+            g = nx.Graph()
+            node_tags = []
+            node_features = []
+            n_edges = 0
+            for j in range(n):
+                g.add_node(j)
+                row = f.readline().strip().split()
+                tmp = int(row[1]) + 2
+                if tmp == len(row):
+                    # no node attributes
+                    row = [int(w) for w in row]
+                    attr = None
+                else:
+                    row, attr = [int(w) for w in row[:tmp]], np.array([float(w) for w in row[tmp:]])
+                if not row[0] in feat_dict:
+                    mapped = len(feat_dict)
+                    feat_dict[row[0]] = mapped
+                node_tags.append(feat_dict[row[0]])
+
+                if tmp > len(row):
+                    node_features.append(attr)
+
+                n_edges += row[1]
+                for k in range(2, len(row)):
+                    g.add_edge(j, row[k])
+
+            if node_features != []:
+                node_features = np.stack(node_features)
+                node_feature_flag = True
+            else:
+                node_features = None
+                node_feature_flag = False
+
+            assert len(g) == n
+
+            g_list.append(S2VGraph(g, l, node_tags))
+
+    #add labels and edge_mat       
+    for g in g_list:
+        g.neighbors = [[] for i in range(len(g.g))]
+        for i, j in g.g.edges():
+            g.neighbors[i].append(j)
+            g.neighbors[j].append(i)
+        degree_list = []
+        for i in range(len(g.g)):
+            g.neighbors[i] = g.neighbors[i]
+            degree_list.append(len(g.neighbors[i]))
+        g.max_neighbor = max(degree_list)
+
+        g.label = label_dict[g.label]
+
+        edges = [list(pair) for pair in g.g.edges()]
+        edges.extend([[i, j] for j, i in edges])
+
+        deg_list = list(dict(g.g.degree(range(len(g.g)))).values())
+        g.edge_mat = torch.LongTensor(edges).transpose(0,1)
+
+    if degree_as_tag:
+        for g in g_list:
+            g.node_tags = list(dict(g.g.degree).values())
+
+    #Extracting unique tag labels   
+    tagset = set([])
+    for g in g_list:
+        tagset = tagset.union(set(g.node_tags))
+
+    tagset = list(tagset)
+    tag2index = {tagset[i]:i for i in range(len(tagset))}
+
+    for g in g_list:
+        g.node_features = torch.zeros(len(g.node_tags), len(tagset))
+        g.node_features[range(len(g.node_tags)), [tag2index[tag] for tag in g.node_tags]] = 1
+
+
+    print('# classes: %d' % len(label_dict))
+    print('# maximum node tag: %d' % len(tagset))
+
+    print("# data: %d" % len(g_list))
+
+    return g_list, len(label_dict)
+
+def separate_data(graph_list, seed, fold_idx):
+    assert 0 <= fold_idx and fold_idx < 10, "fold_idx must be from 0 to 9."
+    skf = StratifiedKFold(n_splits=10, shuffle = True, random_state = seed)
+
+    labels = [graph.label for graph in graph_list]
+    idx_list = []
+    for idx in skf.split(np.zeros(len(labels)), labels):
+        idx_list.append(idx)
+    train_idx, test_idx = idx_list[fold_idx]
+    print(test_idx)
+
+    train_graph_list = [graph_list[i] for i in train_idx]
+    test_graph_list = [graph_list[i] for i in test_idx]
+
+    return train_graph_list, test_graph_list
+
+def separate_TUDataset(graph_list, seed, fold_idx):
+    assert 0 <= fold_idx and fold_idx < 10, "fold_idx must be from 0 to 9."
+    skf = StratifiedKFold(n_splits=10, shuffle = True, random_state = seed)
+
+    labels = [int(graph_list[i].y) for i in range(len(graph_list))]
+    idx_list = []
+    for idx in skf.split(np.zeros(len(labels)), labels):
+        idx_list.append(idx)
+    train_idx, test_idx = idx_list[fold_idx]
+
+    train_graph_list = [graph_list[int(i)] for i in train_idx]
+    test_graph_list = [graph_list[int(i)] for i in test_idx]
+
+    return train_graph_list, test_graph_list
+
+
+def get_dataset(name, sparse=True, cleaned=False):
+    dataset = TUDataset(root='/tmp/' + name, name=name)
+    dataset.data.edge_attr = None
+
+    if dataset.data.x is None:
+        max_degree = 0
+        degs = []
+        for data in dataset:
+            degs += [degree(data.edge_index[0], dtype=torch.long)]
+            max_degree = max(max_degree, degs[-1].max().item())
+
+        if max_degree < 1000:
+            dataset.transform = T.OneHotDegree(max_degree)
+        else:
+            deg = torch.cat(degs, dim=0).to(torch.float)
+            mean, std = deg.mean().item(), deg.std().item()
+            dataset.transform = NormalizedDegree(mean, std)
+
+    if not sparse:
+        num_nodes = max_num_nodes = 0
+        for data in dataset:
+            num_nodes += data.num_nodes
+            max_num_nodes = max(data.num_nodes, max_num_nodes)
+
+        # Filter out a few really large graphs in order to apply DiffPool.
+        if name == 'REDDIT-BINARY':
+            num_nodes = min(int(num_nodes / len(dataset) * 1.5), max_num_nodes)
+        else:
+            num_nodes = min(int(num_nodes / len(dataset) * 5), max_num_nodes)
+
+        indices = []
+        for i, data in enumerate(dataset):
+            if data.num_nodes <= num_nodes:
+                indices.append(i)
+        dataset = dataset.copy(torch.tensor(indices))
+
+        if dataset.transform is None:
+            dataset.transform = T.ToDense(num_nodes)
+        else:
+            dataset.transform = T.Compose(
+                [dataset.transform, T.ToDense(num_nodes)])
+
+    return dataset
+
